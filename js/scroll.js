47af1969427560d30889fcd3470afd036d4484e5
amandaratio <amandaratio@gmail.com>
2017-09-03 10:48:41 -0700

    smooth scroll basic

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,22 @@
-$('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-            || location.hostname == this.hostname) {
-                    var elem = $(this.hash);
-                    elem = elem.length ? elem : $('[name=' + this.hash.slice(1) +']');
-                    if (elem.length) {
-                        $(elem).animate({
-                            scrollTop: elem.offset().top
-                        }, 1200);
-                        return false;
-                    }
-        }
+$('a[href*=#]:not([href=#])').click(function(event) {
+    if ( location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname) {
+      var elem = $(this.hash);
+      elem = elem.length ? elem : $('[name=' + this.hash.slice(1) + ']');
+      if (elem.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: elem.offset().top
+        }, 1200, function() {
+          var $to = $(elem);
+          $to.focus();
+          if ($to.is(":focus")) {
+            return false;
+          } else {
+            $to.attr('tabindex','-1');
+            $to.focus();
+          };
+        });
+      }
+    }
 });
